c920a571e8a1854bf1f025d29839fa16a69ed60f
Julia Garcia <jg439@njit.edu>
2016-12-19 15:50:33 -0500

    changes so we get SQL

--- index.php
+++ index.php
@@ -1,3 +1,30 @@
+<?php
+session_start();
+require_once("class.user.php");
+$login = new USER();
+
+if($login->is_loggedin()!="")
+{
+	$login->redirect('home.php');
+}
+
+if(isset($_POST['btn-login']))
+{
+	$uname = strip_tags($_POST['txt_uname_email']);
+	$umail = strip_tags($_POST['txt_uname_email']);
+	$upass = strip_tags($_POST['txt_password']);
+
+	if($login->doLogin($uname,$umail,$upass))
+	{
+		$login->redirect('home.php');
+	}
+	else
+	{
+		$error = "Wrong Details !";
+	}
+}
+?>
+
 <!DOCTYPE html>
 <html >
   <head>
--- register.php
+++ register.php
@@ -1,3 +1,63 @@
+<?php
+session_start();
+require_once('class.user.php');
+$user = new USER();
+
+if($user->is_loggedin()!="")
+{
+	$user->redirect('home.php');
+}
+
+if(isset($_POST['btn-signup']))
+{
+	$uname = strip_tags($_POST['txt_uname']);
+	$umail = strip_tags($_POST['txt_umail']);
+	$upass = strip_tags($_POST['txt_upass']);
+
+	if($uname=="")	{
+		$error[] = "provide username !";
+	}
+	else if($umail=="")	{
+		$error[] = "provide email id !";
+	}
+	else if(!filter_var($umail, FILTER_VALIDATE_EMAIL))	{
+	    $error[] = 'Please enter a valid email address !';
+	}
+	else if($upass=="")	{
+		$error[] = "provide password !";
+	}
+	else if(strlen($upass) < 6){
+		$error[] = "Password must be atleast 6 characters";
+	}
+	else
+	{
+		try
+		{
+			$stmt = $user->runQuery("SELECT user_name, user_email FROM users WHERE user_name=:uname OR user_email=:umail");
+			$stmt->execute(array(':uname'=>$uname, ':umail'=>$umail));
+			$row=$stmt->fetch(PDO::FETCH_ASSOC);
+
+			if($row['user_name']==$uname) {
+				$error[] = "sorry username already taken !";
+			}
+			else if($row['user_email']==$umail) {
+				$error[] = "sorry email id already taken !";
+			}
+			else
+			{
+				if($user->register($uname,$umail,$upass)){
+					$user->redirect('sign-up.php?joined');
+				}
+			}
+		}
+		catch(PDOException $e)
+		{
+			echo $e->getMessage();
+		}
+	}
+}
+
+?>
 <!DOCTYPE html>
 <html >
   <head>
