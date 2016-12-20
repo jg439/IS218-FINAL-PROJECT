e1282859d842c9861992a8d9a19601f565b1cacd
Julia Garcia <jg439@njit.edu>
2016-12-20 12:36:05 -0500

    changes to registrationform

--- classes/registrationformview.class.php
+++ classes/registrationformview.class.php
@@ -4,23 +4,32 @@
 
      public function getHTML() {
        $form = '
+
+
            <form action="index.php?controller=registrationController" method="post">
-             <div>
+        <div>
+          <label for="firstname">First Name</label>
+          <input type="text" id="firstname" name="firstname" />
+        </div>
+
+        <div>
+          <label for="lastname">Last Name</label>
+          <input type="text" id="lastname" name="lastname" />
+        </div>
+
+
+        <div>
 	         <label for="username">Username</label>
 	         <input type="text" id="username" name="username" />
 	     </div>
-	     <div>
+
+       <div>
 	         <label for="password">Password</label>
 	         <input type="password" id="password" name="password" />
 	     </div>
        <div>
           <label for="email">Email</label>
           <input type="text" id="email" name="email" />
-      </div>
-
-      <div>
-          <label for="address">Address</label>
-          <input type="text" id="address" name="address" />
       </div>
 
 	     <div class="button">
